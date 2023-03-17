02dc022bc591e9405ad15f5e6cb96df3ebb004ce
Ravindu007 <ravindu0504@gmail.com>
2023-03-17 21:45:26 +0530

    context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { SellerProductContextProvider } from './context/SellerProductContext';
 import { SellerProfileContextProvider } from './context/SellerProfileContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <SellerProfileContextProvider>
     <SellerProductContextProvider>
     <App />
     </SellerProductContextProvider>
     </SellerProfileContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
