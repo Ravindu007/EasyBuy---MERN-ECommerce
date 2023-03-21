42c406cf0571550ff17dbf56729d71895cd7979b
Ravindu007 <ravindu0504@gmail.com>
2023-03-21 12:04:36 +0530

    add business log to the qr code

--- frontend/src/bloackChain/useQRcodeGeneration.js
+++ frontend/src/bloackChain/useQRcodeGeneration.js
@@ -1,4 +1,5 @@
 import QRCode from 'qrcode';
+
 import { useAuthContext } from '../hooks/authHooks/useAuthContext';
 import { useSellerProductContext } from '../hooks/useSellerProductContext';
 
@@ -8,7 +9,7 @@ export const useQRcodeGeneration = () => {
   const {user} = useAuthContext()
   const {dispatch} = useSellerProductContext()
 
-  const generateQR = async (blockChainId, product) => {
+  const generateQR = async (blockChainId, product, business) => {
   
     const canvas = document.createElement('canvas');
     const opts = {
@@ -24,7 +25,23 @@ export const useQRcodeGeneration = () => {
     
     // QR code contains the blockchain unique id
     await QRCode.toCanvas(canvas, blockChainId, opts);
+
+
+    // get canvas context
+    const ctx = canvas.getContext("2d");
+
+    // Load the image and draw it onto the canvas using createImageBitmap
+    const img = await fetch(business.businessLogo).then((response) => response.blob());
+    const bitmap = await createImageBitmap(img);
+    const imageWidth = 50;
+    const imageHeight = 50;
+    const qrCodeCenter = canvas.width / 2; // calculate the center of the QR code
+    const imageX = qrCodeCenter - imageWidth / 2;
+    const imageY = qrCodeCenter - imageHeight / 2;
+    ctx.drawImage(bitmap, imageX, imageY, imageWidth, imageHeight);
+
   
+
     // Convert canvas to Blob object => then it fill be the same as normal file
     const blob = await new Promise((resolve) => canvas.toBlob(resolve, 'image/png'));
   
