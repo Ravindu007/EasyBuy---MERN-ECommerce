529a65595cc02691b6c26d2cf54043d6dfe032cd
Ravindu007 <ravindu0504@gmail.com>
2023-07-19 20:26:25 +0530

    backend API change

--- src/hooks/authHooks/useLogin.js
+++ src/hooks/authHooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch("/api/authUsers/login",{
+    const response = await fetch("https://easyproof-backend.onrender.com/api/authUsers/login",{
       method:"POST",
       body:JSON.stringify({email, password}),
       headers:{
