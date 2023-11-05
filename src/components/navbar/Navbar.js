5b3544cac64f598ede17c372af800ae269b64d52
Ravindu007 <ravindu0504@gmail.com>
2023-11-05 14:56:32 +0530

    navbar have to fix when collapse

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,13 +14,13 @@ const Navbar = ({isAdmin}) => {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link to="/" className='navbar-brand'>EasyBuy</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
                 <Link to="/" className='nav-link'>Home</Link>
@@ -65,7 +65,7 @@ const Navbar = ({isAdmin}) => {
          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
-     </div>
+     {/* </div> */}
     </nav>
     </>
   )
